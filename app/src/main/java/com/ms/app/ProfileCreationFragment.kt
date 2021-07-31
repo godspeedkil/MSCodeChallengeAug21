7133fcef9c56d66177f2ee0705b795bff1267997
Edgar Diaz <edgd@amazon.com>
2021-07-31 00:55:19 -0700

    Profile creation fragment layout-ing

--- app/src/main/java/com/ms/app/ProfileCreationFragment.kt
+++ app/src/main/java/com/ms/app/ProfileCreationFragment.kt
@@ -6,17 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.ms.app.databinding.FragmentFirstBinding
+import com.ms.app.databinding.ProfileCreationFragmentBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * Shown to the user when starting, provides input fields for profile creation
  */
-class FirstFragment : Fragment() {
+class ProfileCreationFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: ProfileCreationFragmentBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,7 +21,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = ProfileCreationFragmentBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +29,10 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.submitButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
     }
 
     override fun onDestroyView() {
